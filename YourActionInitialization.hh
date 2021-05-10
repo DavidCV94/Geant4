aba07373cb6473d82eeae67c61165ed975bb3725
DavidCV94 <83954401+DavidCV94@users.noreply.github.com>
2021-05-10 14:58:32 +0100

    YourActionInitialization.hh

---
+++ YourActionInitialization.hh
@@ -0,0 +1,13 @@
+#ifndef YOURACTIONINITIALIZATION_HH
+#define YOURACTIONINITIALIZATION_HH
+#include "G4VUserActionInitialization.hh"
+class YourDetectorConstruction;
+class YourActionInitialization : public G4VUserActionInitialization {
+  public:
+    YourActionInitialization(YourDetectorConstruction* det);
+    virtual ~YourActionInitialization(); 
+    virtual void Build() const;
+  private:
+  	YourDetectorConstruction* fYourDetector;
+};
+#endif
